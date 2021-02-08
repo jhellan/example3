a3be484f8d4fb3ff0c9e96c5cbb2f3bcad0560fb
Jon K Hellan <jon.kare.hellan@uninett.no>
2021-02-08 13:16:14 +0100

    Low level OpenID Connect example

---
+++ README.md
@@ -0,0 +1,37 @@
+# Lavnivå Python-eksempel på innlogging med Feide
+
+- Trykk på denne knappen: [![Run on Repl.it](https://repl.it/badge/github/jhellan/lesson3)](https://repl.it/github/jhellan/lesson3)
+
+- Logg inn på repl.it med konto fra google, github eller facebook
+
+- I repl.it: Gå inn i filen `main.py`. Finn linjen med `APP_BASE_URI` og bytt ut `USERNAME` med ditt brukernavn i repl.it
+
+- Opprett en klient i Feide. Sett `redirect_uri` til verdien av `APP_BASE_URI` etterfulgt av `/redirect_uri`
+
+- Finn `client_id` i Feide. Finn linjen med `APP_CLIENT_ID` og bytt ut verdien med
+  din `client_id`.
+
+- Finn `client_secret` i Feide. Finn filen `.env` i repl.it, og bytt ut verdien bak `APP_SECRET=` med verdien av `client_secret`.
+
+- Trykk på `Run`-knappen i repl.it
+
+- Etter en stund dukker det opp en minibrowser i repl.it. Vi kan ikke bruke denne, men vi kan gå til samme URL i en annen tab. Logg inn med Feide
+
+- Den lokale sesjonen løper ut etter ett minutt. Det går også an å logge ut ved å gå til samme URL etterfulgt av `/logout`.
+
+NB: `id_token` er signert. For enkelhets skyld viser vi ikke her hvordan vi verifiserer signaturen.
+
+Når vi har logget inn, kommer vi til en side med info som applikasjonen har fått fra Feide:
+
+- access\_token: En streng av hex-sifre som bare Feide kan tolke.
+- id\_token: Dette er jwt med info om bukeren. I Feide inneholder den svært lite.
+  Siden viser token etter dekoding.
+- userinfo: Kommer fra OpenID Connect userinfo-endepunktet.
+- mygroups: Gruppene innlogget bruker er med i. Kommer fra Feides gruppe-API.
+
+Applikasjonen demonstrerer 'authorization code flow'. Gangen er:
+
+- `get_code` redirigerer brukers browser til `authorization_endpoint` hos Feide.
+- Feide autentiserer brukeren hvis ikke allerede innlogget, og redirigerer til denne applikasjonens `/redirect_uri`-endepunkt med autentiseringskode som parameter.
+- `get_token` kaller Feides `token_endpoint` med den koden den nettopp fikk fra Feide, og får tilbake en `access_token` og en `id_token`. Vi lagrer disse i brukersesjonen, og redirigerer til hovedsiden i applikasjonen
+- På hovedsiden ser vi at vi nå er innlogget. Vi henter ut `access_token` og `id_token` fra brukersesjonen. Vi bruker `access_token` til å hente ut brukerinfo og gruppeinfo. Til slutt viser vi frem alt sammen.
---
+++ main.py
@@ -0,0 +1,137 @@
+import datetime
+import logging
+import os
+import time
+import urllib
+import flask
+from flask import Flask, jsonify, session
+import jwt
+import requests
+
+app = Flask(__name__)
+
+APP_BASE_URI = 'https://lesson3.USERNAME.repl.co'
+APP_CLIENT_ID = '4fa2099f-ae7d-4f92-8d4b-2a30b6043d84'
+APP_SECRET = os.getenv('APP_SECRET')
+
+AUTH_PROVIDER_BASE_URI = 'https://auth.dataporten.no'
+AUTH_PROVIDER_NAME = 'Feide'
+GROUPS_BASE_URI = 'https://groups-api.dataporten.no'
+MY_GROUPS_PATH = 'groups/me/groups'
+
+logger = logging.getLogger(__name__)
+
+# Flask web framework configuration
+# See http://flask.pocoo.org/docs/0.12/config/
+app.config.update({
+    'SECRET_KEY': 'flask_session_key',  # make sure to change this!!
+    'JSONIFY_PRETTYPRINT_REGULAR': True,
+    'PERMANENT_SESSION_LIFETIME': datetime.timedelta(minutes=1).total_seconds(),
+})
+
+
+def discover_feide_endpoints():
+    discpoint = AUTH_PROVIDER_BASE_URI + "/.well-known/openid-configuration"
+    logger.info("Discovering Feide endpoints at %s", discpoint)
+    res = requests.get(discpoint)
+    res.raise_for_status()
+    return res.json()
+
+
+oidcconfig = discover_feide_endpoints()
+redirect_uri = APP_BASE_URI + '/redirect_uri'
+
+
+def get_code():
+    params = {
+        'response_type': 'code',
+        'client_id': APP_CLIENT_ID,
+        'redirect_uri': redirect_uri,
+        'scope': 'openid profile email userid-feide groups-edu groups-org groups-other',
+    }
+    authzpoint = oidcconfig['authorization_endpoint']
+    logger.info("Redirecting to %s to authenticate and get authorization code", authzpoint)
+    return flask.redirect(authzpoint + '?' + urllib.parse.urlencode(params))
+
+
+def get_token():
+    tokenpoint = oidcconfig['token_endpoint']
+    code = flask.request.args.get('code')
+    params = {
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri,
+    }
+    now = time.time()
+    logger.info("Calling %s to get access token and ID token", tokenpoint)
+    res = requests.post(tokenpoint, auth=(APP_CLIENT_ID, APP_SECRET), data=params)
+    res.raise_for_status()
+    token_response = res.json()
+    access_token = token_response['access_token']
+    scope = token_response['scope']
+    logger.info("Got access_token %s with scope %s", access_token, scope)
+    if token_response.get('id_token'):
+        logger.info("Got id_token - YOU MUST VERIFY!")
+    else:
+        logger.info("Did not get id_token")
+    session['access_token'] = token_response['access_token']
+    session['id_token'] = token_response['id_token']
+    session['expires_at'] = now + token_response['expires_in']
+
+
+def get_userinfo(access_token):
+    infopoint = oidcconfig['userinfo_endpoint']
+    headers={"Authorization": f"Bearer {access_token}"}
+    logger.info("Calling %s to get userinfo", infopoint)
+    response = requests.get(infopoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_my_groups(access_token):
+    headers={"Authorization": f"Bearer {access_token}"}
+    response = requests.get(GROUPS_BASE_URI + '/' + MY_GROUPS_PATH, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def logged_in():
+    return 'access_token' in session and session['expires_at'] > time.time()
+
+
+@app.route('/')
+def login():
+    if not logged_in():
+        return get_code()
+
+    access_token = session['access_token']
+    id_token = session['id_token']
+    id_token_payload = jwt.decode(id_token, options={"verify_signature": False})
+    userinfo = get_userinfo(access_token)
+    mygroups = get_my_groups(access_token)
+    return jsonify([
+        {'access_token': access_token},
+        {'id_token': id_token_payload},
+        {'userinfo': userinfo},
+        {'mygroups': mygroups}
+    ])
+
+
+@app.route('/redirect_uri')
+def callback():
+    logger.info('callback received')
+    get_token()
+    return flask.redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('access_token')
+    return "You've logged out"
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    discover_feide_endpoints()
+    app.run(host='0.0.0.0', port=8080)
